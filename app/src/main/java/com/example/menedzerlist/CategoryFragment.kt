11b54225d75194be5d0a34dc8bcbede5cf11b337
ashkanpotasu <dawidpl8@o2.pl>
2022-11-29 11:16:00 +0100

    CategoryFragment

--- app/src/main/java/com/example/menedzerlist/CategoryFragment.kt
+++ app/src/main/java/com/example/menedzerlist/CategoryFragment.kt
@@ -11,7 +11,7 @@ import com.example.menedzerlist.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class CategoryFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
