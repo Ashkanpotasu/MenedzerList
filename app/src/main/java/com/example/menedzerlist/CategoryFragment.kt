403dc72190b76fdb96a8ff83e5d41bbdfe522002
ashkanpotasu <dawidpl8@o2.pl>
2022-11-29 11:22:24 +0100

    FragmentCategoryBinding

--- app/src/main/java/com/example/menedzerlist/CategoryFragment.kt
+++ app/src/main/java/com/example/menedzerlist/CategoryFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.menedzerlist.databinding.FragmentFirstBinding
+import com.example.menedzerlist.databinding.FragmentCategoryBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class CategoryFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCategoryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class CategoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,7 +33,7 @@ class CategoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_CategoryFragment_to_ListFragment)
         }
     }
 
