45f4eb79ed79a4158ac19ce35173660657a78a02
ashkanpotasu <dawidpl8@o2.pl>
2022-11-29 11:17:06 +0100

    ListFragment

--- app/src/main/java/com/example/menedzerlist/ListFragment.kt
+++ app/src/main/java/com/example/menedzerlist/ListFragment.kt
@@ -11,7 +11,7 @@ import com.example.menedzerlist.databinding.FragmentSecondBinding
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class ListFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
