f9bff738569016b129ccb14fe2e9b15c916fcc1c
Dawid Paszko <dawidpl8@o2.pl>
2023-01-05 23:54:07 +0100

    onCreate()

--- app/src/main/java/com/example/menedzerlist/MainActivity.kt
+++ app/src/main/java/com/example/menedzerlist/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.menedzerlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
