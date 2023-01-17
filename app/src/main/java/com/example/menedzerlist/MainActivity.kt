8d37d1f49ebf02bf4382b1c817502b0c19a67f80
ashkanpotasu <dawidpl8@o2.pl>
2023-01-17 11:12:49 +0100

    setupActionBarWithNavController()

--- app/src/main/java/com/example/menedzerlist/MainActivity.kt
+++ app/src/main/java/com/example/menedzerlist/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.menedzerlist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var navController: NavController
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        //setupActionBarWithNavController(this, navController)
+        setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
